e1c220473376dd8dd1f02a3cc70871cde65bb465
Le Tan <tamlokveer@gmail.com>
2016-10-10 17:04:27 +0800

    Set TextCodec to UTF8
    For now QTextEdit seems handle the Chinese correctly.
    
    Signed-off-by: Le Tan <tamlokveer@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "vmainwindow.h"
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    QTextCodec *codec = QTextCodec::codecForName("UTF8");
+    if (codec) {
+        QTextCodec::setCodecForLocale(codec);
+    }
     VMainWindow w;
     w.show();
 
