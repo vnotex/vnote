013b6cecc2c5ea438caf6f260b42137b4faff302
Le Tan <tamlokveer@gmail.com>
2016-11-24 22:49:31 +0800

    install a message handler
    Signed-off-by: Le Tan <tamlokveer@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,32 @@
 #include "utils/vutils.h"
 #include "vsingleinstanceguard.h"
 
+void VLogger(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toUtf8();
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stderr, "Debug:%s (%s:%u)\n", localMsg.constData(), context.file, context.line);
+        break;
+    case QtInfoMsg:
+        fprintf(stderr, "Info:%s (%s:%u)\n", localMsg.constData(), context.file, context.line);
+        break;
+    case QtWarningMsg:
+        fprintf(stderr, "Warning:%s (%s:%u)\n", localMsg.constData(), context.file, context.line);
+        break;
+    case QtCriticalMsg:
+        fprintf(stderr, "Critical:%s (%s:%u)\n", localMsg.constData(), context.file, context.line);
+        break;
+    case QtFatalMsg:
+        fprintf(stderr, "Fatal:%s (%s:%u)\n", localMsg.constData(), context.file, context.line);
+        abort();
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(VLogger);
+
     VSingleInstanceGuard guard;
     if (!guard.tryRun()) {
         return 0;
