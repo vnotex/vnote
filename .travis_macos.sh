cf3cd677343cf987347ab358a07d0ba91c40cdda
Le Tan <tamlokveer@gmail.com>
2017-12-17 13:53:29 +0800

    fix macOS travisCI build

--- .travis_macos.sh
+++ .travis_macos.sh
@@ -2,8 +2,8 @@
 project_dir=$(pwd)
 
 brew update > /dev/null
-brew install qt@5.9
-QTDIR="/usr/local/opt/qt@5.9"
+brew install qt
+QTDIR="/usr/local/opt/qt"
 PATH="$QTDIR/bin:$PATH"
 LDFLAGS=-L$QTDIR/lib
 CPPFLAGS=-I$QTDIR/include
