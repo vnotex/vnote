c1a46033c86b768c9d6307330a5aa99c2eaede5b
Le Tan <tamlokveer@gmail.com>
2018-04-29 08:52:49 +0800

    release v1.15

--- src/vconfigmanager.cpp
+++ src/vconfigmanager.cpp
@@ -17,7 +17,7 @@ const QString VConfigManager::orgName = QString("vnote");
 
 const QString VConfigManager::appName = QString("vnote");
 
-const QString VConfigManager::c_version = QString("1.14");
+const QString VConfigManager::c_version = QString("1.15");
 
 const QString VConfigManager::c_obsoleteDirConfigFile = QString(".vnote.json");
 
