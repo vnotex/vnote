09ae24395df8d5e1879d7c95d8395ce0b1587756
Le Tan <tamlokveer@gmail.com>
2018-10-24 19:56:07 +0800

    marked: upgrade of the Marked break the language keyword of code block
    Change from "lang" to "language".

--- src/resources/marked.js
+++ src/resources/marked.js
@@ -61,10 +61,10 @@ var updateText = function(text) {
     handleToc(needToc);
     insertImageCaption();
     setupImageView();
-    renderMermaid('lang-mermaid');
-    renderFlowchart(['lang-flowchart', 'lang-flow']);
-    renderPlantUML('lang-puml');
-    renderGraphviz('lang-dot');
+    renderMermaid('language-mermaid');
+    renderFlowchart(['language-flowchart', 'language-flow']);
+    renderPlantUML('language-puml');
+    renderGraphviz('language-dot');
     addClassToCodeBlock();
     renderCodeBlockLineNumber();
 
