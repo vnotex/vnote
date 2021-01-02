7692f9f82aa7115956f477ed6bc1d53e7ce6c895
Le Tan <tamlokveer@gmail.com>
2021-01-02 09:53:56 +0800

    WebViewer: support link jump

--- src/widgets/webpage.cpp
+++ src/widgets/webpage.cpp
@@ -1,5 +1,9 @@
 #include "webpage.h"
 
+#include <utils/widgetutils.h>
+#include <core/vnotex.h>
+#include <core/fileopenparameters.h>
+
 using namespace vnotex;
 
 WebPage::WebPage(QWidget *p_parent)
@@ -13,12 +17,17 @@ bool WebPage::acceptNavigationRequest(const QUrl &p_url,
                                       bool p_isMainFrame)
 {
     Q_UNUSED(p_type);
-    if (!p_isMainFrame) {
+    if (p_url.isLocalFile()) {
+        emit VNoteX::getInst().openFileRequested(p_url.toLocalFile(),
+                                                 QSharedPointer<FileOpenParameters>::create());
+        return false;
+    } if (!p_isMainFrame) {
         return true;
     } else if (p_url.scheme() == QStringLiteral("data")) {
         // Qt 5.12 and above will trigger this when calling QWebEngineView::setHtml().
         return true;
     }
 
+    WidgetUtils::openUrlByDesktop(p_url);
     return false;
 }
