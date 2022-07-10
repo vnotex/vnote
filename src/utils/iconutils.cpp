a2cd6ade0e4c0736173c4e5f968f6ed614517220
chendapao <feloxx@163.com>
2022-07-10 12:31:56 +0800

    del space

--- src/utils/iconutils.cpp
+++ src/utils/iconutils.cpp
@@ -79,7 +79,7 @@ QString IconUtils::replaceForegroundOfIcon(const QString &p_iconContent, const Q
 
 bool IconUtils::isMonochrome(const QString &p_iconContent)
 {
-     QRegExp monoRe("#([0-9]{6}|[a-z]{6}|[A-Z]{6})");
+    QRegExp monoRe("#([0-9]{6}|[a-z]{6}|[A-Z]{6})");
     if (p_iconContent.indexOf(monoRe) > -1) {
         return true;
     }
