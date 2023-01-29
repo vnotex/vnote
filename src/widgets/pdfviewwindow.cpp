b2fa57fe9e79913ebfb3d0f873ff7ba98d8e34dd
Le Tan <tamlokveer@gmail.com>
2023-01-29 22:55:46 +0800

    Update src/widgets/pdfviewwindow.cpp

--- src/widgets/pdfviewwindow.cpp
+++ src/widgets/pdfviewwindow.cpp
@@ -120,7 +120,7 @@ void PdfViewWindow::syncEditorFromBuffer()
     if (buffer) {
         const auto url = PathUtils::pathToUrl(buffer->getContentPath());
         // Solution to ASCII problems, like these file names with these symbols # + &.
-        const auto urlStr = url.toString(QUrl::EncodeSpaces).toUtf8().toPercentEncoding();
+        const auto urlStr = QUrl::toPercentEncoding(url.toString(QUrl::FullyDecoded));
         auto templateUrl = PathUtils::pathToUrl(HtmlTemplateHelper::getPdfViewerTemplatePath());
         templateUrl.setQuery("file=" + urlStr);
         m_viewer->setHtml(HtmlTemplateHelper::getPdfViewerTemplate(), templateUrl);
