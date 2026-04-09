8a57df1c950140769a635539662eb1a507e80207
Le Tan <tamlokveer@gmail.com>
2026-04-10 02:05:52 +0700

    test(unitedentry): add FindUnitedEntry unit tests
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ tests/unitedentry/stubs_findunitedentry.cpp
@@ -0,0 +1,104 @@
+#include <QLabel>
+#include <QTreeWidget>
+
+#include <core/configmgr2.h>
+#include <core/coreconfig.h>
+#include <core/services/notebookcoreservice.h>
+#include <gui/services/themeservice.h>
+#include <gui/utils/iconutils.h>
+#include <unitedentry/entrywidgetfactory.h>
+#include <unitedentry/helpunitedentry.h>
+#include <unitedentry/unitedentryalias.h>
+#include <unitedentry/unitedentryhelper.h>
+#include <utils/utils.h>
+#include <widgets/treewidget.h>
+
+using namespace vnotex;
+
+QSharedPointer<QTreeWidget> EntryWidgetFactory::createTreeWidget(int p_columnCount) {
+  auto tree = QSharedPointer<QTreeWidget>::create();
+  tree->setColumnCount(p_columnCount);
+  return tree;
+}
+
+QSharedPointer<QLabel> EntryWidgetFactory::createLabel(const QString &p_info) {
+  return QSharedPointer<QLabel>::create(p_info);
+}
+
+HelpUnitedEntry::HelpUnitedEntry(UnitedEntryMgr *p_mgr, QObject *p_parent)
+    : IUnitedEntry(QStringLiteral("help"), QString(), p_mgr, p_parent) {}
+
+QSharedPointer<QWidget> HelpUnitedEntry::currentPopupWidget() const { return {}; }
+
+void HelpUnitedEntry::initOnFirstProcess() {}
+
+void HelpUnitedEntry::processInternal(
+    const QString &, const std::function<void(const QSharedPointer<QWidget> &)> &) {
+  emit finished();
+}
+
+UnitedEntryAlias::UnitedEntryAlias(const QString &p_name, const QString &p_description,
+                                   const QString &p_value, UnitedEntryMgr *p_mgr, QObject *p_parent)
+    : IUnitedEntry(p_name, p_description, p_mgr, p_parent), m_value(p_value) {
+  Q_UNUSED(m_value);
+}
+
+UnitedEntryAlias::UnitedEntryAlias(const QJsonObject &, UnitedEntryMgr *p_mgr, QObject *p_parent)
+    : IUnitedEntry(QStringLiteral("alias"), QString(), p_mgr, p_parent) {}
+
+QString UnitedEntryAlias::description() const { return IUnitedEntry::description(); }
+
+QJsonObject UnitedEntryAlias::toJson() const { return QJsonObject(); }
+
+void UnitedEntryAlias::initOnFirstProcess() {}
+
+void UnitedEntryAlias::processInternal(
+    const QString &, const std::function<void(const QSharedPointer<QWidget> &)> &) {
+  emit finished();
+}
+
+bool UnitedEntryAlias::isOngoing() const { return false; }
+
+void UnitedEntryAlias::setOngoing(bool) {}
+
+void UnitedEntryAlias::handleAction(Action) {}
+
+QSharedPointer<QWidget> UnitedEntryAlias::currentPopupWidget() const { return {}; }
+
+bool UnitedEntryAlias::isAliasOf(const IUnitedEntry *) const { return false; }
+
+UnitedEntryHelper::UserEntry UnitedEntryHelper::parseUserEntry(const QString &) { return {}; }
+
+const QIcon &UnitedEntryHelper::itemIcon(ItemType, ThemeService *) {
+  static QIcon s_icon;
+  return s_icon;
+}
+
+UnitedEntryHelper::ItemType UnitedEntryHelper::locationTypeToItemType(LocationType) {
+  return UnitedEntryHelper::Other;
+}
+
+QString ThemeService::getIconFile(const QString &p_icon) const { return p_icon; }
+
+QString ThemeService::paletteColor(const QString &) const { return QString(); }
+
+QIcon IconUtils::fetchIcon(const QString &p_iconFile, const QString &) { return QIcon(p_iconFile); }
+
+CoreConfig &ConfigMgr2::getCoreConfig() {
+  static CoreConfig *s = nullptr;
+  Q_ASSERT_X(false, "stub", "ConfigMgr2::getCoreConfig() called in unit test");
+  return *s;
+}
+
+const QJsonArray &CoreConfig::getUnitedEntryAlias() const {
+  static QJsonArray s;
+  return s;
+}
+
+QJsonArray NotebookCoreService::listNotebooks() const { return QJsonArray(); }
+
+void Utils::sleepWait(int) {}
+
+void TreeWidget::selectParentItem(QTreeWidget *) {}
+
+bool TreeWidget::isExpanded(const QTreeWidget *) { return false; }
---
+++ tests/unitedentry/test_findunitedentry.cpp
@@ -0,0 +1,249 @@
+#include <QtTest>
+
+#include <QCommandLineOption>
+#include <QCommandLineParser>
+
+#include <controllers/searchcontroller.h>
+#include <core/nodeidentifier.h>
+#include <core/servicelocator.h>
+#include <unitedentry/findunitedentry.h>
+#include <unitedentry/unitedentrymgr.h>
+
+using namespace vnotex;
+
+namespace tests {
+
+class TestFindUnitedEntry : public QObject {
+  Q_OBJECT
+
+private slots:
+  void testScopeBuffer();
+  void testScopeFolder();
+  void testScopeNotebook();
+  void testScopeAllNotebook();
+
+  void testObjectContent();
+  void testObjectNamePath();
+  void testObjectTag();
+  void testMultiObjectPriority();
+
+  void testCaseSensitive();
+  void testRegex();
+  void testFilePattern();
+  void testUnsupportedFlagsIgnored();
+  void testDefaultValues();
+
+  void testContextGetterSetter();
+
+private:
+  static void setupParser(QCommandLineParser &p_parser);
+};
+
+void TestFindUnitedEntry::setupParser(QCommandLineParser &p_parser) {
+  p_parser.addPositionalArgument(QStringLiteral("keywords"),
+                                 QStringLiteral("Keywords to search for."));
+
+  QCommandLineOption scopeOpt({QStringLiteral("s"), QStringLiteral("scope")},
+                              QStringLiteral("Search scope."), QStringLiteral("search_scope"),
+                              QStringLiteral("notebook"));
+  p_parser.addOption(scopeOpt);
+
+  QCommandLineOption objectOpt({QStringLiteral("b"), QStringLiteral("object")},
+                               QStringLiteral("Search objects."), QStringLiteral("search_objects"));
+  objectOpt.setDefaultValues({QStringLiteral("name"), QStringLiteral("content")});
+  p_parser.addOption(objectOpt);
+
+  QCommandLineOption targetOpt({QStringLiteral("t"), QStringLiteral("target")},
+                               QStringLiteral("Search targets."), QStringLiteral("search_targets"));
+  targetOpt.setDefaultValues({QStringLiteral("file"), QStringLiteral("folder")});
+  p_parser.addOption(targetOpt);
+
+  p_parser.addOption(QCommandLineOption({QStringLiteral("p"), QStringLiteral("pattern")},
+                                        QStringLiteral("Wildcard pattern of files to search."),
+                                        QStringLiteral("file_pattern")));
+  p_parser.addOption(QCommandLineOption({QStringLiteral("c"), QStringLiteral("case-sensitive")},
+                                        QStringLiteral("Search in case sensitive.")));
+  p_parser.addOption(QCommandLineOption({QStringLiteral("r"), QStringLiteral("regular-expression")},
+                                        QStringLiteral("Search by regular expression.")));
+  p_parser.addOption(QCommandLineOption({QStringLiteral("w"), QStringLiteral("whole-word-only")},
+                                        QStringLiteral("Search whole word only.")));
+  p_parser.addOption(QCommandLineOption({QStringLiteral("f"), QStringLiteral("fuzzy-search")},
+                                        QStringLiteral("Do a fuzzy search.")));
+}
+
+void TestFindUnitedEntry::testScopeBuffer() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-s"), QStringLiteral("buffer"),
+                        QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.scope, static_cast<int>(SearchController::Buffers));
+  QCOMPARE(params.keyword, QStringLiteral("myquery"));
+}
+
+void TestFindUnitedEntry::testScopeFolder() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-s"), QStringLiteral("folder"),
+                        QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.scope, static_cast<int>(SearchController::CurrentFolder));
+}
+
+void TestFindUnitedEntry::testScopeNotebook() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-s"), QStringLiteral("notebook"),
+                        QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.scope, static_cast<int>(SearchController::CurrentNotebook));
+}
+
+void TestFindUnitedEntry::testScopeAllNotebook() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-s"),
+                        QStringLiteral("all_notebook"), QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.scope, static_cast<int>(SearchController::AllNotebooks));
+}
+
+void TestFindUnitedEntry::testObjectContent() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-b"), QStringLiteral("content"),
+                        QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.searchMode, static_cast<int>(SearchController::ContentSearch));
+}
+
+void TestFindUnitedEntry::testObjectNamePath() {
+  {
+    QCommandLineParser parser;
+    setupParser(parser);
+    QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-b"), QStringLiteral("name"),
+                          QStringLiteral("myquery")}));
+
+    const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+    QCOMPARE(params.searchMode, static_cast<int>(SearchController::FileNameSearch));
+  }
+
+  {
+    QCommandLineParser parser;
+    setupParser(parser);
+    QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-b"), QStringLiteral("path"),
+                          QStringLiteral("myquery")}));
+
+    const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+    QCOMPARE(params.searchMode, static_cast<int>(SearchController::FileNameSearch));
+  }
+}
+
+void TestFindUnitedEntry::testObjectTag() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-b"), QStringLiteral("tag"),
+                        QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.searchMode, static_cast<int>(SearchController::TagSearch));
+}
+
+void TestFindUnitedEntry::testMultiObjectPriority() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(
+      parser.parse({QStringLiteral("find"), QStringLiteral("-b"), QStringLiteral("name"),
+                    QStringLiteral("-b"), QStringLiteral("content"), QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.searchMode, static_cast<int>(SearchController::ContentSearch));
+}
+
+void TestFindUnitedEntry::testCaseSensitive() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-c"), QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QVERIFY(params.caseSensitive);
+}
+
+void TestFindUnitedEntry::testRegex() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-r"), QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QVERIFY(params.useRegex);
+}
+
+void TestFindUnitedEntry::testFilePattern() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-p"), QStringLiteral("*.md"),
+                        QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.filePattern, QStringLiteral("*.md"));
+}
+
+void TestFindUnitedEntry::testUnsupportedFlagsIgnored() {
+  QCommandLineParser parser;
+  setupParser(parser);
+
+  QTest::ignoreMessage(
+      QtDebugMsg, "FindUnitedEntry: -w is unsupported in SearchController and will be ignored ");
+  QTest::ignoreMessage(
+      QtDebugMsg, "FindUnitedEntry: -f is unsupported in SearchController and will be ignored ");
+  QTest::ignoreMessage(
+      QtDebugMsg, "FindUnitedEntry: -t is unsupported in SearchController and will be ignored ");
+
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("-w"), QStringLiteral("-f"),
+                        QStringLiteral("-t"), QStringLiteral("file"), QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.keyword, QStringLiteral("myquery"));
+  QCOMPARE(params.scope, static_cast<int>(SearchController::CurrentNotebook));
+  QCOMPARE(params.searchMode, static_cast<int>(SearchController::ContentSearch));
+}
+
+void TestFindUnitedEntry::testDefaultValues() {
+  QCommandLineParser parser;
+  setupParser(parser);
+  QVERIFY(parser.parse({QStringLiteral("find"), QStringLiteral("myquery")}));
+
+  const auto params = FindUnitedEntry::mapArgsToSearchParams(parser);
+  QCOMPARE(params.scope, static_cast<int>(SearchController::CurrentNotebook));
+  QCOMPARE(params.searchMode, static_cast<int>(SearchController::ContentSearch));
+  QCOMPARE(params.keyword, QStringLiteral("myquery"));
+  QVERIFY(!params.caseSensitive);
+  QVERIFY(!params.useRegex);
+  QVERIFY(params.filePattern.isEmpty());
+}
+
+void TestFindUnitedEntry::testContextGetterSetter() {
+  ServiceLocator services;
+  UnitedEntryMgr mgr(services);
+
+  const QString notebookId = QStringLiteral("nb-1");
+  NodeIdentifier folderId;
+  folderId.notebookId = notebookId;
+  folderId.relativePath = QStringLiteral("docs/sub");
+
+  mgr.setCurrentNotebookId(notebookId);
+  mgr.setCurrentFolderId(folderId);
+
+  QCOMPARE(mgr.currentNotebookId(), notebookId);
+  QCOMPARE(mgr.currentFolderId(), folderId);
+}
+
+} // namespace tests
+
+QTEST_GUILESS_MAIN(tests::TestFindUnitedEntry)
+#include "test_findunitedentry.moc"
