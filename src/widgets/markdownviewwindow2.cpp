b3fe0e9c64c29f2390c1b5fd0d41152d3b1b8b90
Le Tan <tamlokveer@gmail.com>
2026-09-17 05:55:21 +0800

    Restore readiness callback wrapper for isolated startup trial

--- src/widgets/markdownviewwindow2.cpp
+++ src/widgets/markdownviewwindow2.cpp
@@ -512,8 +512,9 @@ void MarkdownViewWindow2::setupTextEditor() {
   m_previewHelper->editorZoomChanged();
 
   // Connect viewer <-> editor web channel signals.
-  connect(adapter(), &MarkdownViewerAdapter::ready, m_editor->getHighlighter(),
-          &vte::MarkdownHighlighter::updateHighlight);
+  auto *highlighter = m_editor->getHighlighter();
+  connect(adapter(), &MarkdownViewerAdapter::ready, highlighter,
+          [highlighter]() { highlighter->updateHighlight(); });
   connect(m_editor, &MarkdownEditor::htmlToMarkdownRequested, adapter(),
           &MarkdownViewerAdapter::htmlToMarkdownRequested);
   connect(adapter(), &MarkdownViewerAdapter::htmlToMarkdownReady, m_editor,
